0be1efa30fa60e693c8a9214247cf8dabc6ae7e3
plinadev <pavlenkopolina415@gmail.com>
2025-11-06 10:08:09 +0200

    feat: add menu component with routing and styling

---
+++ src/components/chartBox/ChartBox.tsx
@@ -0,0 +1,71 @@
+import { Link } from "react-router-dom";
+import "./chartBox.scss";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
+
+interface ChartBoxProps {
+  icon: string;
+  title: string;
+  number: string | number;
+  chartData: object[];
+  dataKey: string;
+  color: string;
+  percentage: number;
+}
+function ChartBox({
+  icon,
+  title,
+  number,
+  chartData,
+  dataKey,
+  color,
+  percentage,
+}: ChartBoxProps) {
+  return (
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          <img src={icon} alt="" />
+          <span>{title}</span>
+        </div>
+        <h1>{number}</h1>
+        <Link to="#" style={{ color: color }}>
+          View all
+        </Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart width={300} height={100} responsive data={chartData}>
+              <Tooltip
+                contentStyle={{
+                  background: "transparent",
+                  border: "none",
+                }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 60 }}
+              />
+              <Line
+                type="monotone"
+                dataKey={dataKey}
+                stroke={color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {percentage}%
+          </span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChartBox;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,37 @@
+import ChartBox from "../../components/chartBox/ChartBox";
+import TopBox from "../../components/topBox/TopBox";
+import {
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
 import "./home.scss";
 function Home() {
-  return <div className="home">hOME</div>;
+  return (
+    <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4"></div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        {" "}
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box7"></div>
+      <div className="box box8"></div>
+      <div className="box box9"></div>
+    </div>
+  );
 }
 
 export default Home;
